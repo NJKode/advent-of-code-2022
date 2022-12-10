8e8669ab992c4e262e898ce0c60d8bc5c8490c8e
NJKode <nathan.kennedy.1997@gmail.com>
2022-12-10 20:19:38 +1300

    solved 01

--- 01/src/runner.ts
+++ 01/src/runner.ts
@@ -1,4 +1,4 @@
-import { readFileSync } from 'fs';
+import { txt } from './io/p1_input';
 
 export class Runner {
 	constructor() {
@@ -6,12 +6,35 @@ export class Runner {
 	}
 
 	getInput() {
-		const input = readFileSync('./io/p1_input.txt', 'utf8');
-		console.log(input);
+		let nice;
+		console.log(nice);
+		return txt;
 	}
 
 	solve() {
-		console.log('working...');
-		this.getInput();
+		const input: string = this.getInput();
+		
+		const elves: string[] = input.split('\n\n');
+		let maxCalories: number[] = [0, 0, 0];
+
+		elves.forEach((elf) => {
+			const calories: string[] = elf.split('\n');
+			const totalCalories: number = calories.reduce((total: number, snack:string) => {
+				const snackCalories:number = parseInt(snack);
+				return total += snackCalories;
+			}, 0);
+			
+
+			if (totalCalories > maxCalories[2]) {
+				maxCalories[2] = totalCalories;
+				maxCalories.sort((a, b) => b-a);
+			}
+
+		});
+
+		console.log(maxCalories);
+		const finalScore = maxCalories.reduce((sum: number, cal: number) => sum += cal, 0)
+		console.log(finalScore);
+		return finalScore;
 	}
 }
