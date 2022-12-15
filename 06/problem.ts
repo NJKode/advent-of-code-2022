ff02ff9ee9be9cdaebc63afda851bdbb80072191
NJKode <nathan.kennedy.1997@gmail.com>
2022-12-15 18:07:55 +1300

    Solved 06

--- 06/problem.ts
+++ 06/problem.ts
@@ -2,10 +2,30 @@ import { Problem } from '../lib/problem';
 import { input } from './io/input';
 
 export class Problem06 extends Problem {
+
+	findMarkerPosition(bufferSize: number) {
+		let markerPosition: number = 0;
+
+		let start: number = 0;
+		while (!markerPosition && start + bufferSize <= input.length) {
+			const buffer: string = input.substring(start, start + bufferSize);
+
+			if (new Set(Array.from(buffer)).size === bufferSize) {
+				markerPosition = start + bufferSize
+			}
+
+			start += 1;
+		}
+
+		return markerPosition.toString();
+	}
 	
 	solve() {
-		const p1Solution: string = '';
-		const p2Solution: string = '';
+		const packetMarkerSize: number = 4;
+		const messageMarkerSize: number = 14;
+
+		const p1Solution: string = this.findMarkerPosition(packetMarkerSize);;
+		const p2Solution: string = this.findMarkerPosition(messageMarkerSize);;
 
 		return {
 			p1: p1Solution,
