36b995a5e4650ece6dd478442a128fcc3c1760d3
NJKode <nathan.kennedy.1997@gmail.com>
2022-12-15 17:52:28 +1300

    Project boilerplate

---
+++ 22/problem.ts
@@ -0,0 +1,15 @@
+import { Problem } from '../lib/problem';
+import { input } from './io/input';
+
+export class Problem22 extends Problem {
+	
+	solve() {
+		const p1Solution: string = '';
+		const p2Solution: string = '';
+
+		return {
+			p1: p1Solution,
+			p2: p2Solution
+		}
+	}
+}
--- lib/problem-lookup.ts
+++ lib/problem-lookup.ts
@@ -3,15 +3,53 @@ import { Problem02 } from '../02/problem';
 import { Problem03 } from '../03/problem';
 import { Problem04 } from '../04/problem';
 import { Problem05 } from '../05/problem';
-
+import { Problem06 } from '../06/problem';
+import { Problem07 } from '../07/problem';
+import { Problem08 } from '../08/problem';
+import { Problem09 } from '../09/problem';
+import { Problem10 } from '../10/problem';
+import { Problem11 } from '../11/problem';
+import { Problem12 } from '../12/problem';
+import { Problem13 } from '../13/problem';
+import { Problem14 } from '../14/problem';
+import { Problem15 } from '../15/problem';
+import { Problem16 } from '../16/problem';
+import { Problem17 } from '../17/problem';
+import { Problem18 } from '../18/problem';
+import { Problem19 } from '../19/problem';
+import { Problem20 } from '../20/problem';
+import { Problem21 } from '../21/problem';
+import { Problem22 } from '../22/problem';
+import { Problem23 } from '../23/problem';
+import { Problem24 } from '../24/problem';
+import { Problem25 } from '../25/problem';
 
 const lookup = {
 	'01': Problem01,
 	'02': Problem02,
 	'03': Problem03,
 	'04': Problem04,
-	'05': Problem05
-
+	'05': Problem05,
+	'06': Problem06,
+	'07': Problem07,
+	'08': Problem08,
+	'09': Problem09,
+	'10': Problem10,
+	'11': Problem11,
+	'12': Problem12,
+	'13': Problem13,
+	'14': Problem14,
+	'15': Problem15,
+	'16': Problem16,
+	'17': Problem17,
+	'18': Problem18,
+	'19': Problem19,
+	'20': Problem20,
+	'21': Problem21,
+	'22': Problem22,
+	'23': Problem23,
+	'24': Problem24,
+	'25': Problem25
 }
 
 export default lookup;
